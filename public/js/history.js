8ee3d7e94957b377714d2d44e8509b838a0ed7ef
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2022-01-11 09:06:06 +0000

    Javascript Cleanup

--- public/js/history.js
+++ public/js/history.js
@@ -1,22 +1,54 @@
+function getActionPopup(){ return document.getElementById("historyOptionPopup"); }
+function getActionPopupBackground() { return document.getElementById("historyOptionPopupBackground");}
+actionRecordId = 0;
+historyCard = null;
+function updateActionId(id){
+  historyCard = document.getElementById(id);
+  actionRecordId = id;
+}
+
+
+//Actions Popup Tooltip
+document.addEventListener('click', function(e){
+  if (e.target.matches('.historyCardOptionButton, .historyCardOptionButton *')) {
+    var button = e.target.closest(".historyCardOptionButton");
+    var historyCard = button.parentElement.parentElement;
+    updateActionId(historyCard.id);
+
+
+    //Set ViewEdit Popup ID Value
+    document.getElementById("PopupViewEditID").value = actionRecordId;
+
+    //Change Popup Text
+    document.getElementById('PopupBookmarkText').innerText = historyCard.querySelector('.bookmarkIcon').classList.contains("hidden") ? "Bookmark" : "Un-Bookmark";
+      
+    ShowActionPopup(button.offsetLeft, button.offsetTop);
+  }
+  else if(!e.target.matches('#historyOptionPopup, #historyOptionPopup *')){
+    HideActionPopup();
+  }
+});
+
 //Delete Popup Delete
-const deleteClick = (id) => {
-    HideHistoryActionPopup();
+const deleteClick = () => {
     var deletePopup = document.getElementById("deletePopup");
-    document.getElementById('recordIdInput').value = id;
+    document.getElementById('recordIdInput').value = actionRecordId;
     deletePopup.classList.remove("hidden");
+    HideActionPopup();
 }
 
 //Delete Popup Cancel
 const cancelClick = () => {
     var deletePopup = document.getElementById("deletePopup");
+    document.getElementById('recordIdInput').value = 0;
     deletePopup.classList.add("hidden");
 }
 
 //Handle Bookmark
-const bookmarkClick = async (id) => {
-  const bookmark = await fetch('/api/record/bookmark/?recordId='+id);
+const bookmarkClick = async () => {
+  const bookmark = await fetch('/api/record/bookmark/?recordId='+actionRecordId);
   const record = await bookmark.json();
-  var historyCard = document.getElementById(id);
+  
   if(record.bookmarked){
     historyCard.querySelector('.bookmarkIcon').classList.remove("hidden");
     historyCard.querySelector('.bookmarkIcon').classList.add("flex");
@@ -25,7 +57,7 @@ const bookmarkClick = async (id) => {
     historyCard.querySelector('.bookmarkIcon').classList.remove("flex");
     historyCard.querySelector('.bookmarkIcon').classList.add("hidden");
   }
-  HideHistoryActionPopup();
+  HideActionPopup();
 };
 
 
@@ -47,51 +79,35 @@ function pageData(count, page, limit) {
     };
   }
 
-//Actions Popup Tooltip
-document.addEventListener('click', function(e){
-  popup = document.getElementById("historyOptionPopup");
-  popupBackground = document.getElementById("historyOptionPopupBackground");
-  if (e.target.matches('.historyCardOptionButton, .historyCardOptionButton *')) {
-      var button = e.target.closest(".historyCardOptionButton");
-      var historyCard = button.parentElement.parentElement;
-      
-      //Set Popup ID Value
-      document.getElementById("PopupViewEditID").value = historyCard.id;
-      document.getElementById('PopupDeleteButton').setAttribute('onclick','deleteClick(\''+historyCard.id+'\')')
-      document.getElementById('PopupBookmarkButton').setAttribute('onclick','bookmarkClick(\''+historyCard.id+'\')')
 
 
-      //Change Popup Text
-      document.getElementById('PopupBookmarkText').innerText = historyCard.querySelector('.bookmarkIcon').classList.contains("hidden") ? "Bookmark" : "Un-Bookmark";
-      //PopupBookmarkText
-
-      //Show Popup
-      historyCard.classList.add("z-10");
-      popup.classList.remove("hidden");
-
-      //Move Popup
-      popup.style.left = (button.offsetLeft - popup.clientWidth + 20) +"px";
-      popup.style.top = (button.offsetTop - popup.clientHeight) +"px"; 
-
-      //Show Popup Background
-      popupBackground.classList.remove("hidden");
-      
-  }
-  else if(e.target.matches('#historyOptionPopup, #historyOptionPopup *')){
-      //Do nothing
-  }
-  else{
-    HideHistoryActionPopup();
-  }
-});
-
-function HideHistoryActionPopup(){
-  popup = document.getElementById("historyOptionPopup");
-  popupBackground = document.getElementById("historyOptionPopupBackground");
-
-  popup.classList.add("hidden");
-  popupBackground.classList.add("hidden");
+function HideActionPopup(){
+  
+  HideActionPopupOnly();
+  //actionRecordId = 0;
 
   //Remove z-10 from all history cards:
   Array.from(document.getElementsByClassName('historyCard')).forEach((el) => el.classList.remove('z-10'));
+}
+
+function HideActionPopupOnly(){
+  popup = getActionPopup();
+  popup.classList.add("hidden");
+  popupBackground = getActionPopupBackground();
+  popupBackground.classList.add("hidden");
+}
+
+function ShowActionPopup(offsetLeft, offsetTop){
+  //Show Popup
+  historyCard.classList.add("z-10");
+  getActionPopup().classList.remove("hidden");
+
+  popup = getActionPopup();
+
+  //Move Popup
+  popup.style.left = (offsetLeft - popup.clientWidth + 20) +"px";
+  popup.style.top = (offsetTop - popup.clientHeight) +"px";
+    
+  //Show Popup Background
+  getActionPopupBackground().classList.remove("hidden");
 }
